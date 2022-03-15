29dc0c7c907c075758092148ab3f409cae1c9465
Nitinmpatil <101519863+Nitinmpatil@users.noreply.github.com>
2022-03-15 09:52:41 +0530

    commit1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //do
+            //{
+            //    Console.WriteLine("1. New File");
+            //    Console.WriteLine("2. Open File");
+            //    Console.WriteLine("3. Save File");
+            //    Console.WriteLine("4. Save As File");
+            //    Console.WriteLine("5. Exit");
+
+            //    int option = Convert.ToInt32(Console.ReadLine());
+
+            //    if(option >= 1 && option <=5)
+            //    {
+            //        Console.WriteLine("Correct Option");
+            //        break;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Choose a correct option");
+            //    }
+            //}while(true);
+
+            //Console.ReadKey();
+            //Console.WriteLine();
+            int x = 100;
+            do
+            {
+                Console.WriteLine("X is: {0}", x);
+                x = x - 1;
+            } while (x > 0);
+        }
+    }
+}
